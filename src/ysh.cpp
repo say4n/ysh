a9ddac8561bd5a908ee2e429e110324e424a312d
Sayan Goswami <goswami.sayan47@gmail.com>
2019-08-14 04:07:40 +0530

    Add shell builtins

--- src/ysh.cpp
+++ src/ysh.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <vector>
+#include <map>
 #include "rang.hpp"
 
 #define DEBUG false
@@ -9,23 +10,41 @@
 using namespace std;
 using namespace rang;
 
+typedef int (*builtin_func)(vector<string>);
+
 
 char *convert(const string& );
-void executeCommand(vector<string>);
+int executeCommand(vector<string>);
+int launchCommand(vector<string>);
 void prompt(string*);
 void processArgs(vector<string>*, string);
+
+// ysh built-ins
+map<string, builtin_func> builtins;
+int ysh_cd(vector<string>);
+int ysh_pwd(vector<string>);
+int ysh_exit(vector<string>);
+int ysh_help(vector<string>);
 
 
 int main() {
     string command;
     vector<string> args;
+    int status = true;
 
-    while (true) {
+    builtins = {
+        {"cd", &ysh_cd},
+        {"pwd", &ysh_pwd},
+        {"help", &ysh_help},
+        {"exit", &ysh_exit},
+    };
+
+    while (status) {
         prompt(&command);
 
         processArgs(&args, command);
 
-        executeCommand(args);
+        status = executeCommand(args);
 
         command.clear();
         args.clear();
@@ -52,7 +71,9 @@ void processArgs(vector<string>* args, string command) {
 
     for(auto ch: command) {
         if (ch == ' ' | ch == '\0' | ch == '\n') {
+            interim.push_back('\0');
             args->push_back(interim);
+
             interim.clear();
         }
         else {
@@ -69,13 +90,25 @@ void processArgs(vector<string>* args, string command) {
 }
 
 
-void executeCommand(vector<string> args) {
+int executeCommand(vector<string> args) {
+    if (args[0][0] == '\0') {
+        // empty input
+        return 1;
+    }
+
+    for(auto b: builtins) {
+        if (strcmp(b.first.c_str(), args[0].c_str()) == 0) {
+            return (*b.second)(args);
+        }
+    }
+
+    return launchCommand(args);
+}
+
+
+int launchCommand(vector<string> args) {
     pid_t pid, wpid;
     int status;
-    bool should_wait = args.at(args.size() - 1) != "&";
-    if (!should_wait) {
-        args.pop_back();
-    }
 
     vector<char*> cargs;
     transform(args.begin(), args.end(), std::back_inserter(cargs), convert);
@@ -102,13 +135,13 @@ void executeCommand(vector<string> args) {
     }
     else {
         // parent process
-        if (should_wait) {
-            // wait() unless command has &
-            do {
-                wpid = waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
+        // wait() unless command has &
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+
+    return 1;
 }
 
 
@@ -117,5 +150,49 @@ char *convert(const std::string & s)
    char *pc = new char[s.size()+1];
    std::strcpy(pc, s.c_str());
    return pc;
+}
+
+
+int ysh_cd(vector<string> args) {
+    if (args[1] == "\0") {
+        if (chdir("~") != 0) {
+            perror("error changing directory to ~");
+        }
+    }
+    else {
+        if (chdir(convert(args[1])) != 0) {
+            perror("error changing directory");
+        }
+    }
+
+    return 1;
+}
+
+
+int ysh_pwd(vector<string> args) {
+    string pwd = getcwd(NULL, 0);
+    cout << pwd << endl;
+
+    return 1;
+}
+
+
+int ysh_exit(vector<string> args) {
+    if (DEBUG) cerr << "exit called\n";
+    return 0;
+}
+
+
+int ysh_help(vector<string> args) {
+    cout << fg::yellow << "ysh" << style::reset << ": yet another shell\n";
+    cout << fg::yellow << "author" << style::reset << ": @say4n\n";
+    cout << fg::yellow <<"builtins" << style::reset << ": ";
+
+    for (auto b: builtins) {
+        cout << b.first << ", ";
+    }
+    cout << "\b\b.\n";
+
+    return 1;
 }
 
